e4a35f43a7dd41258adb67653a9a9267a5f66dca
Theodor <ftri2001@gmail.com>
2021-10-28 13:41:13 +0300

    Added solution for 'maximum number of prizes' task

--- 1-algorithmic-toolbox/week3_programming_challenges/6_maximum_number_of_prizes/6_maximum_number_of_prizes.cpp
+++ 1-algorithmic-toolbox/week3_programming_challenges/6_maximum_number_of_prizes/6_maximum_number_of_prizes.cpp
@@ -1,20 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <cassert>
+#include <cmath>
 
-using std::vector;
+std::vector<int32_t> OptimalSummands(int32_t candiesCount) {
+    auto solveForCandies = [](int32_t n) -> float {
+        return (-1 + std::sqrt(1 + 8 * n)) / 2.0f;
+    };
 
-vector<int> optimal_summands(int n) {
-  vector<int> summands;
-  //write your code here
-  return summands;
+    auto nSum = [](int32_t n) -> float {
+        return (n * (n + 1)) / 2.0f;
+    };
+
+    float upperBound = solveForCandies(candiesCount);
+
+    std::vector<int32_t> summands { };
+
+    // The candies count is a perfect number
+    if (std::floor(upperBound) == upperBound) {
+        summands.resize(static_cast<uint32_t>(std::floor(upperBound)));
+        std::iota(std::begin(summands), std::end(summands), 1);
+
+        return summands;
+    }
+
+    summands.resize(static_cast<uint32_t>(std::floor(upperBound)) - 1);
+    std::iota(std::begin(summands), std::end(summands), 1);
+    summands.push_back(candiesCount - nSum(std::floor(upperBound)) + std::floor(upperBound));
+
+    return summands;
+}
+
+std::vector<int> CorrectSolution(int n) {
+    // using the algorithm described in the pdf
+    std::vector<int> summands;
+    for (int k = n, l = 1; k > 0; l++) {
+        if (k <= 2 * l) {
+            summands.push_back(k);
+            k -= k;
+        } else {
+            summands.push_back(l);
+            k -= l;
+        }
+    }
+    return summands;
+}
+
+void CheckSolution() {
+    struct ProblemStatement {
+        std::vector<int32_t> summands;
+        int32_t candiesCount;
+    };
+
+    std::vector<ProblemStatement> problemSolutionPairs {
+        ProblemStatement{ .summands = { 1, 2, 3 }, .candiesCount{ 6 }},
+        ProblemStatement{ .summands = { 1, 2, 5 }, .candiesCount{ 8 }},
+        ProblemStatement{ .summands = { 1 }, .candiesCount{ 1 }},
+        ProblemStatement{ .summands = { 2 }, .candiesCount{ 2 }}
+    };
+
+    for (auto& testcase : problemSolutionPairs) {
+        if (OptimalSummands(testcase.candiesCount) != testcase.summands) {
+            throw std::runtime_error("Incorrect answer!");
+        }
+    }
+
+    for (int32_t i = 1; i < 10'000'000; i++) {
+        std::vector<int32_t> a = std::move(OptimalSummands(i));
+        std::vector<int32_t> b = std::move(CorrectSolution(i));
+        if (a != b) {
+            std::cerr << "Errored index: " << i << std::endl;
+            throw std::runtime_error("Incorrect answer!");
+        }
+    }
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> summands = optimal_summands(n);
-  std::cout << summands.size() << '\n';
-  for (size_t i = 0; i < summands.size(); ++i) {
-    std::cout << summands[i] << ' ';
-  }
+    int32_t n;
+    std::cin >> n;
+    std::vector<int32_t> summands = std::move(CorrectSolution(n));
+    std::cout << summands.size() << '\n';
+    for (size_t i = 0; i < summands.size(); ++i) {
+        std::cout << summands[i] << ' ';
+    }
+
+    CheckSolution();
 }

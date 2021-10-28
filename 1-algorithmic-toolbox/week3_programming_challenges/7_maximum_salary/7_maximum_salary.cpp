b9735fb9dbc537bfe30b4a1b09f6ad18592540a3
Theodor <ftri2001@gmail.com>
2021-10-28 15:07:11 +0300

    Added solution for 'maximum salary' task

--- 1-algorithmic-toolbox/week3_programming_challenges/7_maximum_salary/7_maximum_salary.cpp
+++ 1-algorithmic-toolbox/week3_programming_challenges/7_maximum_salary/7_maximum_salary.cpp
@@ -1,29 +1,81 @@
 #include <algorithm>
-#include <sstream>
+#include <iterator>
 #include <iostream>
 #include <vector>
 #include <string>
 
-using std::vector;
-using std::string;
 
-string largest_number(vector<string> a) {
-  //write your code here
-  std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
-  }
-  string result;
-  ret >> result;
-  return result;
+bool CompareByDigits(const std::string& firstNum, const std::string& secondNum)
+{
+    auto firstNumIter = firstNum.begin();
+    auto secondNumIter = secondNum.begin();
+
+    for (uint32_t i = 0; i < std::max(firstNum.length(), secondNum.length()); ++i) {
+        if (*firstNumIter > *secondNumIter) {
+            return true;
+        } else if (*firstNumIter < *secondNumIter) {
+            return false;
+        }
+
+        // If the first number isn't 'over' yet, advance the iterator
+        if (firstNumIter + 1 != firstNum.end()) {
+            firstNumIter++;
+        }
+
+        // If the second number isn't 'over' yet, advance the iterator
+        if (secondNumIter + 1 != secondNum.end()) {
+            secondNumIter++;
+        }
+    }
+
+    // If numbers are equal, return true.
+    return true;
+}
+
+
+std::string LargestNumber(std::vector<std::string>& numbers) {
+    std::string result;
+    result.reserve(numbers.size());
+
+    std::sort(numbers.begin(), numbers.end(), CompareByDigits);
+
+    for (size_t i = 0; i < numbers.size(); i++) {
+        result += numbers[i];
+    }
+
+    return result;
+}
+
+
+void CheckSolution() {
+    struct ProblemStatement {
+        std::vector<std::string> numbers;
+        std::string answer;
+    };
+
+    std::vector<ProblemStatement> problemSolutionPairs {
+        ProblemStatement{ .numbers = { "21", "2" }, .answer{ "221" }},
+        ProblemStatement{ .numbers = { "9", "4", "6", "1", "9" }, .answer{ "99641" }},
+        ProblemStatement{ .numbers = { "23", "39", "92" }, .answer{ "923923" }},
+        ProblemStatement{ .numbers = { "29", "249", "237", "2" }, .answer{ "292492372" }},
+        ProblemStatement{ .numbers = { "200", "2", "210" }, .answer{ "2210200" }}
+    };
+
+    for (auto& testcase : problemSolutionPairs) {
+        if (LargestNumber(testcase.numbers) != testcase.answer) {
+            throw std::runtime_error("Incorrect answer!");
+        }
+    }
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<string> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  std::cout << largest_number(a);
+    // int32_t n;
+    // std::cin >> n;
+    // std::vector<std::string> numbers(n);
+    // for (size_t i = 0; i < numbers.size(); i++) {
+    //     std::cin >> numbers[i];
+    // }
+    // std::cout << LargestNumber(numbers);
+
+    CheckSolution();
 }

b4f008e6bdecb452b00d9372d855cdcbb1212e17
Theodor <ftri2001@gmail.com>
2022-01-16 23:11:22 +0300

    Added solution for the primitive calculater task

--- 1-algorithmic-toolbox/week5_programming_challenges/2_primitive_calculator/primitive_calculator.cpp
+++ 1-algorithmic-toolbox/week5_programming_challenges/2_primitive_calculator/primitive_calculator.cpp
@@ -2,30 +2,108 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
+std::vector<int32_t> OptimalSequence(int32_t number) {
+    std::vector<int32_t> minOperations(number + 1);
+    
+    for (int32_t curNum = 2; curNum <= number; ++curNum) {
+        int32_t curMin = std::numeric_limits<int32_t>::max();        
 
-vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+        if (curNum % 2 == 0 && minOperations[curNum / 2] + 1 < curMin) {
+            curMin = minOperations[curNum / 2] + 1;
+        }
+
+        if (curNum % 3 == 0 && minOperations[curNum / 3] + 1 < curMin) {
+            curMin = minOperations[curNum / 3] + 1;
+        }
+
+        if (curNum >= 1 && minOperations[curNum - 1] + 1 < curMin) {
+            curMin = minOperations[curNum - 1] + 1;
+        }
+
+        minOperations[curNum] = curMin;
     }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+    
+    std::vector<int32_t> sequence {};
+    sequence.reserve(minOperations[number]);
+    sequence.push_back(number);
+
+    // Reconstruct optimal solution
+    for (int32_t curNum = number; curNum > 1; ) {
+        int32_t curMin = std::numeric_limits<int32_t>::max();        
+        int32_t curNumCopy = curNum;
+
+        sequence.push_back(curNum);
+
+        if (curNum % 2 == 0 && minOperations[curNum / 2] < curMin) {
+            curMin = minOperations[curNum / 2];
+            sequence.back() = curNum / 2;
+            curNumCopy = curNum / 2;
+        }
+
+        if (curNum % 3 == 0 && minOperations[curNum / 3] < curMin) {
+            curMin = minOperations[curNum / 3];
+            sequence.back() = curNum / 3;
+            curNumCopy = curNum / 3;
+        }
+
+        if (curNum >= 1 && minOperations[curNum - 1] < curMin) {
+            curMin = minOperations[curNum - 1];
+            sequence.back() = curNum - 1;
+            curNumCopy = curNum - 1;
+        }
+
+        curNum = curNumCopy;
+    }
+
+    std::reverse(std::begin(sequence), std::end(sequence));
+
+    return sequence;
+}
+
+bool CheckSolution() {
+    struct ProblemStatement {
+        int32_t number;
+        int32_t optimalSequenceLen;
+        std::string name;
+    };
+
+    std::vector<ProblemStatement> problemSolutionPairs {
+        ProblemStatement{ .number = 1, .optimalSequenceLen = 0, .name{ "1 - testcase" } },
+        ProblemStatement{ .number = 5, .optimalSequenceLen = 3, .name{ "2 - testcase" } },
+        ProblemStatement{ .number = 96234, .optimalSequenceLen = 14, .name{ "3 - testcase" } },
+    };
+
+    for (auto& testcase : problemSolutionPairs) {
+        auto myAlgoAns = OptimalSequence(testcase.number);
+        if (myAlgoAns.size() != testcase.optimalSequenceLen) {
+            throw std::runtime_error("Got        : " + std::to_string(myAlgoAns.size()) + ".\n"
+                                     "Expected   : " + std::to_string(testcase.optimalSequenceLen) + ".\n"
+                                     "On testcase: " + testcase.name + "\n");
+        }
+    }
+
+    // for (auto n : { 4, 13, 24, 34 }) {
+    //     auto correctAnswer = GetChangeSlow(n);
+    //     auto myAlgoAns = GetChange(n);
+    //     if (correctAnswer != myAlgoAns) {
+    //         throw std::runtime_error("Got: " + std::to_string(myAlgoAns) + ".\n"
+    //                                  "Expected: " + std::to_string(correctAnswer) + ".\n");
+    //     }
+    // }
+
+    return true;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
-  }
+    // if (CheckSolution()) {
+    //     std::cout << "The solution is correct!\n";
+    // }
+
+    int32_t n;
+    std::cin >> n;
+    std::vector<int32_t> sequence = OptimalSequence(n);
+    std::cout << sequence.size() - 1 << std::endl;
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        std::cout << sequence[i] << " ";
+    }
 }

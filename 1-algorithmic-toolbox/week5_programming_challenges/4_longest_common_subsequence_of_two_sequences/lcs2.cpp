6cde60c062aba64112f6e0eaaeae7b6316db3203
Theodor <ftri2001@gmail.com>
2022-01-17 13:14:26 +0300

    Added solution for the longest common subsequence-2 problem

--- 1-algorithmic-toolbox/week5_programming_challenges/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ 1-algorithmic-toolbox/week5_programming_challenges/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -3,8 +3,27 @@
 
 int32_t LongestCommonSubsequence2(std::vector<int32_t>& a, std::vector<int32_t>& b)
 {
-    // write your code here
-    return std::min(a.size(), b.size());
+    // Initial DP matrix values
+    std::vector<std::vector<int32_t>> dpMatrix(a.size() + 1, std::vector<int32_t>(b.size() + 1));
+
+    for (uint32_t rowIdx = 1; rowIdx <= a.size(); ++rowIdx) {
+        for (uint32_t colIdx = 1; colIdx <= b.size(); ++colIdx) {
+            int32_t curMax = std::numeric_limits<int32_t>::min();
+
+            if (a[rowIdx - 1] == b[colIdx - 1] && dpMatrix[rowIdx - 1][colIdx - 1] + 1 > curMax)
+                curMax = dpMatrix[rowIdx - 1][colIdx - 1] + 1;
+
+            if (a[rowIdx - 1] != b[colIdx - 1] && dpMatrix[rowIdx - 1][colIdx] > curMax)
+                curMax = dpMatrix[rowIdx - 1][colIdx];
+
+            if (a[rowIdx - 1] != b[colIdx - 1] && dpMatrix[rowIdx][colIdx - 1] > curMax)
+                curMax = dpMatrix[rowIdx][colIdx - 1];
+
+            dpMatrix[rowIdx][colIdx] = curMax;
+        }
+    }
+
+    return dpMatrix[a.size()][b.size()];
 }
 
 bool CheckSolution()
@@ -22,6 +41,10 @@ bool CheckSolution()
         ProblemStatement{ .seq1{ 7 }, .seq2{ 1, 2, 3, 4 }, .answer = 0, .name{ "2 - testcase" } },
         ProblemStatement{
             .seq1{ 2, 7, 8, 3 }, .seq2{ 5, 2, 8, 7 }, .answer = 2, .name{ "3 - testcase" } },
+        ProblemStatement{ .seq1{ 1, 2, 3, 3, 2, 2, 2, 2, 4, 6, 7, 8, 9 },
+                          .seq2{ 1, 2, 3, 6, 8, 2, 5, 3, 4, 7, 5, 8, 0, 8, 1, 6, 7, 8, 9 },
+                          .answer = 9,
+                          .name{ "4 - testcase" } },
     };
 
     for (auto& testcase : problemSolutionPairs) {

c3194b09dad1497fa3399d704ad8175195d4c740
Theodor <ftri2001@gmail.com>
2022-01-17 12:26:26 +0300

    Added clang-format to automatically format the code

--- 1-algorithmic-toolbox/week5_programming_challenges/3_edit_distance/edit_distance.cpp
+++ 1-algorithmic-toolbox/week5_programming_challenges/3_edit_distance/edit_distance.cpp
@@ -5,16 +5,15 @@
 /**
  * @brief Calculates EditDistance between two strings str1 and str2.
  * Uses bottom-up DP approach
- * 
- * @param str1 
- * @param str2 
- * @return int32_t 
+ *
+ * @param str1
+ * @param str2
+ * @return int32_t
  */
-int32_t EditDistance(const std::string& str1, const std::string& str2) {
-    std::vector<std::vector<int32_t>> dpMat(
-        str1.length() + 1, 
-        std::vector<int32_t>(str2.length() + 1)
-    );
+int32_t EditDistance(const std::string& str1, const std::string& str2)
+{
+    std::vector<std::vector<int32_t>> dpMat(str1.length() + 1,
+                                            std::vector<int32_t>(str2.length() + 1));
 
     // Initial DP matrix values
     for (uint32_t i = 0; i <= str1.size(); i++)
@@ -26,15 +25,15 @@ int32_t EditDistance(const std::string& str1, const std::string& str2) {
     for (uint32_t rowIdx = 1; rowIdx <= str1.length(); ++rowIdx) {
         for (uint32_t colIdx = 1; colIdx <= str2.length(); ++colIdx) {
             int32_t curMin = std::numeric_limits<int32_t>::max();
-            
+
             // Deletion
             if (dpMat[rowIdx - 1][colIdx] + 1 < curMin)
                 curMin = dpMat[rowIdx - 1][colIdx] + 1;
-            
+
             // Insertion
             if (dpMat[rowIdx][colIdx - 1] + 1 < curMin)
                 curMin = dpMat[rowIdx][colIdx - 1] + 1;
-                
+
             // Substitution
             if (str1[rowIdx - 1] != str2[colIdx - 1] && dpMat[rowIdx - 1][colIdx - 1] + 1 < curMin)
                 curMin = dpMat[rowIdx - 1][colIdx - 1] + 1;
@@ -50,48 +49,57 @@ int32_t EditDistance(const std::string& str1, const std::string& str2) {
     return dpMat[str1.length()][str2.length()];
 }
 
-bool CheckSolution() {
-    struct ProblemStatement {
+bool CheckSolution()
+{
+    struct ProblemStatement
+    {
         std::string str1;
         std::string str2;
         int32_t answer;
         std::string name;
     };
 
-    std::vector<ProblemStatement> problemSolutionPairs {
+    std::vector<ProblemStatement> problemSolutionPairs{
         ProblemStatement{ .str1{ "ab" }, .str2{ "ab" }, .answer = 0, .name{ "1 - testcase" } },
-        ProblemStatement{ .str1{ "short" }, .str2{ "ports" }, .answer = 3, .name{ "2 - testcase" } },
-        ProblemStatement{ .str1{ "editing" }, .str2{ "distance" }, .answer = 5, .name{ "3 - testcase" } },
+        ProblemStatement{
+            .str1{ "short" }, .str2{ "ports" }, .answer = 3, .name{ "2 - testcase" } },
+        ProblemStatement{
+            .str1{ "editing" }, .str2{ "distance" }, .answer = 5, .name{ "3 - testcase" } },
         ProblemStatement{ .str1{ "cat" }, .str2{ "cut" }, .answer = 1, .name{ "4 - testcase" } },
-        ProblemStatement{ .str1{ "sunday" }, .str2{ "saturday" }, .answer = 3, .name{ "5 - testcase" } },
-        ProblemStatement{ .str1{ "ATGTTATA" }, .str2{ "ATCGTCC" }, .answer = 5, .name{ "6 - testcase" } },
-        ProblemStatement{ 
-            .str1{ "aacudfoennnndpperdsdpernskfytskkjjjfornthshgf" }, 
-            .str2{ "ssacudfoenndpeeprerdsddnskfytskkjjjfoppthsggf" }, 
-            .answer = 13, 
-            .name{ "7 - testcase" }
-        },
-        ProblemStatement{ 
-            .str1{ "aacudfoennnndpperdsdpernskfytskkjjjforntdkihkjhkaysduiasydhshgfasudaiuyasdadiuastdgjbegjuagsjhdasd" }, 
-            .str2{ "sdeudfoenfffdpperdsweernskfytskkjjjforntdkihkjhkasydhshgfasudasdjfgwhghjqyasdadiuastdgjbegjuagsjdjihsgjdwsd" }, 
-            .answer = 32, 
-            .name{ "8 - testcase" }
-        }
+        ProblemStatement{
+            .str1{ "sunday" }, .str2{ "saturday" }, .answer = 3, .name{ "5 - testcase" } },
+        ProblemStatement{
+            .str1{ "ATGTTATA" }, .str2{ "ATCGTCC" }, .answer = 5, .name{ "6 - testcase" } },
+        ProblemStatement{ .str1{ "aacudfoennnndpperdsdpernskfytskkjjjfornthshgf" },
+                          .str2{ "ssacudfoenndpeeprerdsddnskfytskkjjjfoppthsggf" },
+                          .answer = 13,
+                          .name{ "7 - testcase" } },
+        ProblemStatement{ .str1{ "aacudfoennnndpperdsdpernskfytskkjjjforntdkihkjhkaysduiasydhshgfas"
+                                 "udaiuyasdadiuastdgjbegjuagsjhdasd" },
+                          .str2{ "sdeudfoenfffdpperdsweernskfytskkjjjforntdkihkjhkasydhshgfasudasdj"
+                                 "fgwhghjqyasdadiuastdgjbegjuagsjdjihsgjdwsd" },
+                          .answer = 32,
+                          .name{ "8 - testcase" } }
     };
 
     for (auto& testcase : problemSolutionPairs) {
         auto myAlgoAns = EditDistance(testcase.str1, testcase.str2);
         if (myAlgoAns != testcase.answer) {
-            throw std::runtime_error("Got        : " + std::to_string(myAlgoAns) + ".\n"
-                                     "Expected   : " + std::to_string(testcase.answer) + ".\n"
-                                     "On testcase: " + testcase.name + "\n");
+            throw std::runtime_error("Got        : " + std::to_string(myAlgoAns) +
+                                     ".\n"
+                                     "Expected   : " +
+                                     std::to_string(testcase.answer) +
+                                     ".\n"
+                                     "On testcase: " +
+                                     testcase.name + "\n");
         }
     }
 
     return true;
 }
 
-int main() {
+int main()
+{
     if (CheckSolution()) {
         std::cout << "The solution is correct!\n";
     }
--- 1-algorithmic-toolbox/week5_programming_challenges/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ 1-algorithmic-toolbox/week5_programming_challenges/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
-
-int lcs2(vector<int> &a, vector<int> &b) {
-  //write your code here
-  return std::min(a.size(), b.size();
+int32_t LongestCommonSubsequence2(std::vector<int32_t>& a, std::vector<int32_t>& b)
+{
+    // write your code here
+    return std::min(a.size(), b.size());
 }
 
-int main() {
-  size_t n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> a[i];
-  }
+bool CheckSolution()
+{
+    struct ProblemStatement
+    {
+        std::vector<int32_t> seq1;
+        std::vector<int32_t> seq2;
+        int32_t answer;
+        std::string name;
+    };
 
-  size_t m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (size_t i = 0; i < m; i++) {
-    std::cin >> b[i];
-  }
+    std::vector<ProblemStatement> problemSolutionPairs{
+        ProblemStatement{ .seq1{ 2, 7, 5 }, .seq2{ 2, 5 }, .answer = 2, .name{ "1 - testcase" } },
+        ProblemStatement{ .seq1{ 7 }, .seq2{ 1, 2, 3, 4 }, .answer = 0, .name{ "2 - testcase" } },
+        ProblemStatement{
+            .seq1{ 2, 7, 8, 3 }, .seq2{ 5, 2, 8, 7 }, .answer = 2, .name{ "3 - testcase" } },
+    };
 
-  std::cout << lcs2(a, b) << std::endl;
+    for (auto& testcase : problemSolutionPairs) {
+        auto myAlgoAns = LongestCommonSubsequence2(testcase.seq1, testcase.seq2);
+        if (myAlgoAns != testcase.answer) {
+            throw std::runtime_error("Got        : " + std::to_string(myAlgoAns) +
+                                     ".\n"
+                                     "Expected   : " +
+                                     std::to_string(testcase.answer) +
+                                     ".\n"
+                                     "On testcase: " +
+                                     testcase.name + "\n");
+        }
+    }
+
+    return true;
+}
+
+int32_t main()
+{
+    if (CheckSolution()) {
+        std::cout << "The solution is correct!\n";
+    }
+
+    std::size_t n;
+    std::cin >> n;
+    std::vector<int32_t> a(n);
+    for (size_t i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    size_t m;
+    std::cin >> m;
+    std::vector<int32_t> b(m);
+    for (size_t i = 0; i < m; i++) {
+        std::cin >> b[i];
+    }
+
+    std::cout << LongestCommonSubsequence2(a, b) << std::endl;
 }
